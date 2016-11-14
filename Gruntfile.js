1152a278bd52f5b89cb9c8dc646915a4a99c9a9e
Vagner Santos <santos.vagnerferreira@gmail.com>
2016-11-14 10:03:18 -0200

    init project

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt){
+	grunt.initConfig({
+		
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-browser-sync');
+}
